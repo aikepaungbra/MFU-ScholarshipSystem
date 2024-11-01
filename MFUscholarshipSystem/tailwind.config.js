4235695354ec5eeaee9e90872c45c06b1306b405
TAYZAR TUN <6631503089@lamduan.mfu.ac.th>
2024-11-01 20:49:11 +0700

    kit 10.2

---
+++ MFUscholarshipSystem/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: [
+    './src/main/resources/templates/**/*.html', // Include all HTML files in the templates folder
+    './src/main/resources/static/**/*.js', // If you have any JS files
+  ],
+  // ...other Tailwind configurations
+}
